6fbd57c6426caadaaae311165e05db81d3c15ed9
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2024-05-27 22:25:25 +0200

    update state

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -12,7 +12,7 @@ from models.state import State
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
 def get_states():
     """retrieve the list of all states"""
-    states = storage.all()
+    states = storage.all(State).values()
     return make_response(jsonify([state.to_dict() for
                          state in states.values()]), 200)
 
@@ -20,7 +20,7 @@ def get_states():
 @app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
 def get_state(state_id):
     """retrieve one state by its id"""
-    state = storage.get('State', state_id)
+    state = storage.get(State, state_id)
     if not state:
         abort(404)
     return make_response(jsonify({state.to_dict}), 200)
@@ -30,11 +30,11 @@ def get_state(state_id):
                  methods=['DELETE'],
                  strict_slashes=False)
 def delete_state(state_id):
-    state = storage.get('State', state_id)
+    state = storage.get(State, state_id)
     if not state:
         abort(404)
-    state.delete()
-    state.save()
+    storage.delete(state)
+    storage.save()
     return make_response(jsonify({}), 200)
 
 
@@ -48,14 +48,17 @@ def add_state():
         abort(400, 'Missing name')
     state = State(**body)
     storage.new(state)
-    storage.save
+    storage.save()
     return make_response(jsonify(state.to_dict), 201)
 
 
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
 def update_state(state_id):
     """updates a state object"""
-    state = storage.get('State', state_id)
+    state = storage.get(State, state_id)
+        if not state:
+    abort(404)
+
     body = request.get_json()
     if not body:
         abort(400, 'Not a JSON')
@@ -65,4 +68,4 @@ def update_state(state_id):
         if key not in ignored_values:
             setattr(state, key, value)
     state.save()
-    return make_response(jsonify(state.to_dict), 200)
+    return make_response(jsonify(state.to_dict()), 200)
