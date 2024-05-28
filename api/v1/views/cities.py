03bc39fff9cde539dfaea0b7ffe2bf9e56e40451
AhmedNewiry <ahmedabdalfttah@outlook.com>
2024-05-29 00:42:01 +0200

    fix

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """a module containing the routes of cities view"""
 
+from api.v1.views import app_views
 from models import storage
 from flask import make_response, request, abort, jsonify
 from models.city import City
